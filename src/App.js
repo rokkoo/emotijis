0c2e0857af7f6e8fcbb9f750cfa8f594a77a2479
Alfonso <alfonsolasaguirre@gmail.com>
2019-05-31 14:50:09 +0200

    Mostramos las predicciones en pantalla

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React from "react";
-import useVideo from "./useVideo";
-import happy from "./happy.jpg";
+import React, { useEffect } from 'react';
+import { css } from '@emotion/core';
+import { CircleLoader } from 'react-spinners';
 
-import "./styles.css";
+import useVideo from './useVideo';
+import happy from './happy.jpg';
+import male from './male.jpg';
+
+import './styles.css';
+
+const DrawAtribute = ({ text, value }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'start',
+        alignItems: 'center'
+      }}
+    >
+      <h5 style={{ marginRight: '3px' }}>{text}:</h5> {value}
+    </div>
+  );
+};
+
+const getBestExpression = expressions =>
+  Object.keys(expressions).reduce((prev, current) =>
+    expressions[prev] > expressions[current] ? prev : current
+  );
 
 function UseVideoComponent() {
   //const [video, videoStream] = useVideo();
   const [img, callback] = useVideo();
   //<video ref={video} width="300" height="300" />
 
+  useEffect(() => {
+    // WE have prediction data
+    if (callback !== null) {
+      console.log(callback.expressions);
+    }
+  }, [callback]);
+
+  const override = css``;
+
   return (
-    <>
-      {callback}
-      <img ref={img} width="300" height="300" src={happy} alt="" hidden />
-    </>
+    <div className="imgContainer">
+      <section className="picture">
+        <img ref={img} width="310" height="310" src={male} alt="" />
+      </section>
+      {callback !== null ? (
+        <section className="atributes">
+          <DrawAtribute text="Age" value={callback.age} />
+          <DrawAtribute text="Gender" value={callback.gender} />
+          <DrawAtribute
+            text="Gender"
+            value={getBestExpression(callback.expressions)}
+          />
+        </section>
+      ) : (
+        <section className="loadingModel">
+          <CircleLoader
+            css={override}
+            sizeUnit={'px'}
+            size={90}
+            color={'black'}
+            loading={true}
+          />
+        </section>
+      )}
+    </div>
   );
 }
 
--- src/useVideo.js
+++ src/useVideo.js
@@ -1,40 +1,43 @@
-import React, { useEffect, useState } from "react";
-import * as faceapi from "face-api.js";
+import React, { useEffect, useState } from 'react';
+import * as faceapi from 'face-api.js';
 
 const useVideo = () => {
   const [element, setElement] = useState(null);
-  const [canvas, setCanvas] = useState(<canvas></canvas>);
+  const [detections, setDetections] = useState(null);
 
   const loadModels = async () => {
-    const MODEL_URL = process.env.PUBLIC_URL + "/models";
-    console.log(MODEL_URL);
-    await faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    const MODEL_URL = process.env.PUBLIC_URL + '/models';
+    await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
     await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
     await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
     await faceapi.loadFaceRecognitionModel(MODEL_URL);
-    await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
-    await faceapi.nets.ageGenderNet.loadFromUri('/models')
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.ageGenderNet.loadFromUri('/models');
   };
-
 
   useEffect(() => {
     const predicElement = async () => {
       await loadModels();
-  
+
       // let description = await faceapi
       // .detectAllFaces(element)
       // .withFaceLandmarks(true)
       // .withFaceDescriptors();
-  
-      const detenctions = await faceapi.detectSingleFace(element).withFaceLandmarks().withAgeAndGender().withFaceDescriptor()
 
+      const detections = await faceapi
+        .detectSingleFace(element)
+        .withFaceLandmarks()
+        .withFaceExpressions()
+        .withAgeAndGender()
+        .withFaceDescriptor();
+
+      setDetections(detections);
       // const displaySize = { width: element.width, height: element.height }
       // faceapi.matchDimensions(canvas, displaySize)
       // // resize the detected boxes in case your displayed image has a different size than the original
       // const resizedDetections = faceapi.resizeResults(detenctions, displaySize)
       // setCanvas(faceapi.draw.drawDetections(canvas, resizedDetections))
-
-      console.log(detenctions)
     };
 
     if (element) {
@@ -42,7 +45,7 @@ const useVideo = () => {
     }
   }, [element]);
 
-  return [setElement, canvas];
+  return [setElement, detections];
 };
 
 export default useVideo;
